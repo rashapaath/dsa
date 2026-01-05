71d782ecaf1e6fe908d36c2b81feaddf90a6b87f
rashapaath <rashawin25@gmail.com>
2026-01-05 21:55:14 +0530

    Time: 139 ms (5.06%), Space: 47.3 MB (49.02%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,6 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int l=nums[0];
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>l){
-                l=nums[i];
-            }
-        }
-        for(int i=0;i<=l;i++){
+        for(int i=0;i<=nums.length;i++){
             int flag=0;
             for(int j=0;j<nums.length;j++){
                 if(i==nums[j]){
@@ -18,7 +12,6 @@ class Solution {
                 return i;
             }
         }
-        return l + 1;
-       
+       return -1;
     }
 }

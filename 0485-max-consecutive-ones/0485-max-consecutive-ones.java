3d05aabe2556944fc8472c904e94d7aa1c65810c
rashapaath <rashawin25@gmail.com>
2026-01-06 00:49:09 +0530

    Time: 3 ms (57.44%), Space: 52.4 MB (62.26%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -12,6 +12,7 @@ class Solution {
                 count=0;
             }
         }
+        countlist.add(count);
         int countmax=countlist.get(0);
         for(int i=0;i<countlist.size();i++){
             if(countmax<countlist.get(i)){

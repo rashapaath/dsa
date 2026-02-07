641662f81e6281cbf1e0a0fdc5a419dc3e2b0d3a
rashapaath <rashawin25@gmail.com>
2026-02-07 23:12:38 +0530

    Time: 0 ms (100%), Space: 43.4 MB (9.66%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,16 +1,33 @@
 class Solution {
-    public boolean check(int[] nums) {
-        int count = 0;
-        int n = nums.length;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > nums[(i + 1) % n]) {
+    public boolean check(int[] nums) { 
+        //condition for sort
+        //arr[i]<=arr[i+1](0 to n-2)this will not break 
+        //since rotation allowed therefore one break at any point occurs
+        //also one  more condition 
+        //arr[n-1]<=arr[0]
+        //inorder to make considering this condition too therefor to check whether the rotated(or not) array is sorted or not
+        //1)arr[i]<=arr[i+1%n] 
+        //if no break sorted unrotated array
+        //if one break sorted rotaated array
+        //if more than one break unsorted array
+        boolean check=true;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>nums[(i+1)%nums.length]){
+                check=false;
                 count++;
-                if (count > 1) {
-                    return false;
-                }
             }
+
         }
-        return true;
+        if(check==true){
+            return true;
+        }
+        else if(count==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
     }
 }

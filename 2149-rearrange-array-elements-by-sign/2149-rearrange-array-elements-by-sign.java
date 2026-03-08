223b39ebeaebfead1304dff105543d24e17d9fb8
Fathima rasha <rashawin25@gmail.com>
2026-03-08 13:48:10 +0530

    Time: 10 ms (7.48%), Space: 174.4 MB (83.59%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,26 +1,30 @@
+import java.util.ArrayList;
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int[] arranged=new int[nums.length];
-        //1)initialising indices
-        int posindex=0;
-        int negindex=1;
-        //2)iterating array and adding elements to positive and negetive indices
-        for(int num:nums){
-            if(num>=0){
-                arranged[posindex]=num;
-                posindex+=2;
-
-            }
-            else if(num<0){
-                arranged[negindex]=num;
-                negindex+=2;
-
+        int n=nums.length;
+        ArrayList<Integer> poseven=new ArrayList<>();
+        ArrayList<Integer> negodd=new ArrayList<>();
+        for(int i=0;i< nums.length;i++){
+            if(nums[i]>=0){
+                poseven.add(nums[i]);
             }
             else{
-
+                negodd.add(nums[i]);
             }
         }
-        return arranged;
+        int k=0;
+        int l=0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                nums[i]=poseven.get(k++);
+            }
+            else{
+                nums[i]=negodd.get(l++);
+            }
+        }
+        return nums;
+
+        
         
     }
 }

36b2291d3c9ebd70cfa643c1c9179001aa197a9d
Fathima rasha <rashawin25@gmail.com>
2026-07-18 23:59:04 +0530

    Time: 1 ms (99.86%), Space: 47.3 MB (96.28%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,54 +1,18 @@
-import java.util.ArrayList;
 class Solution {
-    
     public void moveZeroes(int[] nums) {
-       
-            /*brute
-        //tc=o(2n)
-    //sc=o(k)=o(n)
-        ArrayList<Integer> list=new ArrayList<>();
-        
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                list.add(nums[i]);
-                           }
+        int n = nums.length;
+        int[] temp = new int[n];
+
+        int k = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0)
+                temp[k++] = nums[i];
         }
-        int i=0;
-        for(int elt:list){
-            nums[i]=elt;
-            i++;
-        }
-        for(int j=i;j<nums.length;j++){
-            nums[j]=0;
-        }
-        //better two pointers
-        tc=o(2n);
-        sc=o(1)
-         int i=0;
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]!=0){
-                nums[i]=nums[j];
-                i++;
-            }
-        }
-        while(i<nums.length) {
-            nums[i]=0;
-            i++;
-        }
-        */
-        /*
-        optimal
-        tc=
-        sc=
-        */
-        int i=0;
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]!=0){
-                int temp=nums[j];
-                nums[j]=nums[i];
-                nums[i]=temp;
-                i++;
-            }
+
+        // Copy back
+        for (int i = 0; i < n; i++) {
+            nums[i] = temp[i];
         }
     }
 }

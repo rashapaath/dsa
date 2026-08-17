8b808ecc06634b504a26b35e7f043302288de3c8
Fathima rasha <rashawin25@gmail.com>
2026-08-18 00:44:55 +0530

    Time: 5 ms (98.15%), Space: 46.8 MB (85.2%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,18 +1,27 @@
-import java.util.TreeMap;
 class Solution {
     public int firstUniqChar(String s) {
-        TreeMap<Integer,Integer> map=new TreeMap<>();
-        for(int i=0;i<s.length();i++){
-            int ch=s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+        int n=s.length();
+        int[] freqarr=new int[26];
+        for(int i=0;i<26;i++){
+            freqarr[i]=-1;
         }
-        for(int i=0;i<s.length();i++){
-            int ch=s.charAt(i);
-            if(map.get(ch)==1){
-                return i;
+        for(int i=0;i<n;i++) {
+            int index = s.charAt(i) - 'a';
+            if (freqarr[index] == -1) {
+                freqarr[index] = i;
+            } else {
+                freqarr[index] = -2;
             }
         }
-        return -1;
-        
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<26;i++){
+            if(freqarr[i]>=0){
+                min=Math.min(min,freqarr[i]);
+            }
+        }
+        if(min==Integer.MAX_VALUE){
+            return -1;
+        }
+        return min;
     }
 }
